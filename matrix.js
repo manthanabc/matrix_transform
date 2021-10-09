81f1029a5960e4fe3d08ca0e9b0d68920cbbd531
manthanabc <manthanmji@gmail.com>
2021-10-09 21:53:09 +0530

    changes

--- matrix.js
+++ matrix.js
@@ -2,8 +2,8 @@ let i = [0, 1];
 let j = [1, 0];
 let resolution = 30;
 
-let ixslider ;
-let iyslider ;
+let ixInput ;
+let iyInput ;
 
 let jxslider ;
 let jyslider ;
@@ -12,20 +12,25 @@ let resSlider ;
 
 function setup() {
   createCanvas(500, 500);
-  background(180);
+  background(200);
+
+  strokeWeight(0);
+  textSize(12);
+  textStyle(NORMAL);
+  text('resolution', 10, 15);
   
   resSlider = createSlider(5, 80, 30, 0.1);
   resSlider.position(width + 50, 50);
   resSlider.style('width', '80px');
   
-  ixslider = createSlider(-1, 1, 0, 0.01);
-  ixslider.position(width + 50, 100);
-  ixslider.style('width', '80px');
+  ixInput = createSlider(-1, 1, 0, 0.01);
+  ixInput.position(width + 50, 100);
+  ixInput.style('width', '80px');
   
-  iyslider = createSlider(0, 2, 1, 0.01);
-  iyslider.position(width + 50, 150);
-  iyslider.style('width', '80px');
-
+  iyInput = createSlider(-1, 2, 1, 0.01);
+  iyInput.position(width + 50, 150);
+  iyInput.style('width', '80px');
+  iyInput.input(iyinput);
 
   jxslider = createSlider(0, 2, 1, 0.01);
   jxslider.position(width + 50, 250);
@@ -43,7 +48,7 @@ function draw() {
   translate(width/2, height/2);
   scale(1,-1);
   
-  background(180);
+  background(50);
   
   point(0, 0);
  
@@ -53,7 +58,8 @@ function draw() {
   
   strokeWeight(1);
   stroke(100);
-  let px, py = 0;
+  let px = 0;
+  let py = 0;
   
   for(let x=-width/2 + adjustX;x < width/2; x+=resolution) {
     for(let y=-height/2 + adjustY; y< height/2; y+=resolution) {
@@ -68,7 +74,7 @@ function draw() {
        let [pnx, pny] = transform(x, y-1*resolution, i, j);
        let [tnx, tny] = transform(x-1*resolution, y, i, j);
        strokeWeight(1);
-       stroke(10,55,255);
+       stroke(50 ,190 , 50);
        point(nx , ny);
        line(pnx, pny, nx, ny);
        line(tnx, tny, nx, ny);
@@ -84,13 +90,14 @@ function draw() {
   line(0, height/2, 0, -height/2);
   line(width/2, 0, -width/2, 0);
   
-  i[0]= ixslider.value();
-  i[1] = iyslider.value();
+  i[0] = ixInput.value();
+  //i[0] = 0;
+  i[1] = iyInput.value();
   
-  // j[0] = jxslider.value();
-  // j[1] = jyslider.value();
-  j[0] = i[1];
-  j[1] = i[0];
+  j[0] = jxslider.value();
+  j[1] = jyslider.value();
+  //j[0] = i[1];
+  //j[1] = i[0];
   
   resolution = resSlider.value();
 }
@@ -99,4 +106,11 @@ function transform(x, y, i, j) {
   let nx = j[0] * x + i[0] * y;
   let ny = j[1] * x + i[1] * y;
   return [nx, ny];
+}
+
+function iyinput() {
+  if (this.value){
+    i[1] = pareseInt(this.value);
+  }
+    //i[0] = parseInt(this.value);
 }
